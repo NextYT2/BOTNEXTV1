7b50745df6cde4e16124a4651f4e8984383c94d0
NextYT2 <77639415+NextYT2@users.noreply.github.com>
2021-01-18 17:03:01 -0300

    Add files via upload

---
+++ info.js
@@ -0,0 +1,44 @@
+exports.info = (id, A187, tampilTanggal, whatsapp, youtube, tampilWaktu, instagram, nomer, aktif) => {
+	return ` 
+❉──────────❉
+
+*${A187}*
+
+❉──────────❉
+
+📆 *${tampilTanggal}*
+📌STATUS BOT ATIVO: *${aktif}*
+
+         
+╔════════════════════
+║ *Feito por: $Bruno*
+╠════════════════════
+║   *⚠️ATENÇÃO⚠️*
+╠════════════════════
+║*OS CRÉDITOS NAO SÃO MEUS*
+║*APENAS FIZ UMA VERSÃO INSTÁVEL*
+╠════════════════════
+║╭──❉ *INFO* ❉──
+║│➸ *AUTOR*: Bruno
+║│➸ *DESIGNER*: Bruno
+║│➸ *YOUTUBE*: NEXT FF ツ
+║╰───────────
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│➸ *Faça login aqui😙*
+║│ _${11946343140}_
+║│➸ *YouTube*
+║│ _${NEXT FF ツ}_
+║│➸ *Instagram <@obruninhowendell>*
+║│ _${instagram}_
+║│➸ *Criador*
+║│    Bruno ツ
+║╰───────────
+║         ${A187}
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║         ${A187}
+╠════════════════════
+║ _*Feito por Bruno ツ*_
+╚════════════════════`
+}
---
+++ menu2.js
@@ -0,0 +1,49 @@
+exports.menu2 = (id, A187, tampilTanggal, whatsapp, youtube, tampilWaktu, instagram, nomer, aktif) => {
+	return ` 
+❉──────────❉
+
+𝐍𝐄𝐗𝐓 ツ
+
+❉──────────❉
+
+ 
+ 𝐍𝐄𝐗𝐓 𝐃𝐎𝐌𝐈𝐍𝐀 🐦🖤 𝐎𝐋𝐇𝐄 𝐎𝐒 𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 𝐀𝐁𝐀𝐈𝐗𝐎
+	
+╔════════════════════
+║ *MENU2*    
+╠════════════════════
+║╭──❉ *CITAÇÕES E PUISI* ❉──
+║│➸ _*!puisi1*_
+║│➸ _*!puisi2*_
+║│➸ _*!puisi3*_
+║│➸ _*!cerpen*_
+║│➸ _*!quotes1*_
+║│➸ _*!quotes2*_
+║╰──────────
+╠════════════════════
+║╭──❉ *BAIXAR* ❉──
+║│➸ _*!ytmp3*_  <https://youtu.be/8nZyRHznzC8>
+║│➸ _*!ytmp4*_ <https://youtu.be/8nZyRHznzC8>
+║│➸ _*!fb*_ <Bruno Wendell>
+║│➸ _*!tiktok*_  <OFF>
+║│➸ _*!twt*_ <link>
+║│➸ _*!ig*_ <link>
+║╰──────────
+║ *NEXT ${FF}*
+╠════════════════════
+║
+║
+║
+║
+║╭──❉ *18+* ❉──
+║│➸ _*!xvideos.com*_
+║│➸ _*!cersex*_
+║│➸ _*!hentai*_
+║│➸ _*!indohot*_
+║│➸ _*!nekonime*_
+║╰───────────
+
+╠════════════════════
+║ 𝐍𝐄𝐗𝐓 ツ 𝐃𝐎𝐌𝐈𝐍𝐀 🐦🖤
+╚════════════════════`
+}
